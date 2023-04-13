d6369ee1df4cf2033a068eb6f8ede38368bc6a13
Eduardo Aguiar <jose.aguiar@inpeaceapp.com>
2023-04-13 17:10:39 -0300

    transfer client push to service worker

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-navigator.serviceWorker.register('sw.js')
-  // Send push notification
-  .then(registration => {
-    const btn = document.getElementById('btn');
-    btn.addEventListener('click', () => {
-      registration.showNotification('Hello world!', {
-        body: 'This is a notification',
-      });
-    });
-  });
-
 // Ativar notificações
 const btn = document.getElementById('btn');
 
@@ -19,70 +8,75 @@ const message = document.getElementById('message');
 const icon = document.getElementById('icon');
 const image = document.getElementById('image');
 
-// Verifica se o navegador suporta notificações
-btn.addEventListener('click', () => {
-  Notification.requestPermission().then(perm => {
-    if (perm === 'granted') {
-      const notification = new Notification('Notificações Ativa', {
-        body: 'Você receberá notificações',
-      });
-    } else {
-      console.log('Permission denied');
-    }
+// Send push notification
+navigator.serviceWorker.register('sw.js')
+  .then(registration => {
+    // Ativa notificações
+    btn.addEventListener('click', () => {
+      Notification.requestPermission().then(perm => {
+        if (perm === 'granted') {
+          registration.showNotification('Hello world!', {
+            body: 'This is a notification',
+          });
+        } else {
+          console.log('Permission denied');
+        }
+      })
+    });
+
+    // Envia notificação quando a pagina sai do foco
+    let notification;
+    let interval;
+    document.addEventListener('visibilitychange', () => {
+      if (document.visibilityState === 'hidden') {
+        const leave = new Date();
+        interval = setInterval(() => {
+          notification = registration.showNotification('Saiu da pagina', {
+            body: `Você está fora da pagina a ${Math.round((new Date() - leave) / 1000)} segundos. Volte logo!`,
+            tag: 'leave',
+          });
+        }, 100);
+      } else {
+        if (interval) clearInterval(interval);
+        registration.getNotifications({ tag: 'leave' }).then(notifications => {
+          notifications.forEach(notification => notification.close());
+        });
+      }
+    });
   });
-});
 
-// Envia notificação quando a pagina sai do foco
-let notification;
-let interval
-document.addEventListener('visibilitychange', () => {
-  if (document.visibilityState === 'hidden') {
-    const leave = new Date()
-    interval = setInterval(() => {
-      notification = new Notification('Saiu da pagina', {
-        body: `Você está fora da pagina a ${Math.round((new Date() - leave) / 1000)} segundos. Volte logo!`,
-        tag: 'leave',
-      });
-    }, 100);
+// // Envia notificação quando a pagina é fechada
+// window.onbeforeunload = () => {
+//   notification = new Notification('Fechou a pagina', {
+//     body: 'Volte logo!',
+//   });
+// };
 
-  } else {
-    if (interval) clearInterval(interval)
-    if (notification) notification.close()
-  }
-});
+// // Envia notificação quando a pagina é carregada
+// window.onload = () => {
+//   Notification.requestPermission().then(perm => {
+//     if (perm === 'granted') {
+//       const notification = new Notification('Bem vindo de volta!', {
+//         body: 'Que bom que voltou!',
+//       });
+//     } else {
+//       console.log('Permission denied');
+//     }
+//   });
+// };
 
-// Envia notificação quando a pagina é fechada
-window.onbeforeunload = () => {
-  notification = new Notification('Fechou a pagina', {
-    body: 'Volte logo!',
-  });
-};
-
-// Envia notificação quando a pagina é carregada
-window.onload = () => {
-  Notification.requestPermission().then(perm => {
-    if (perm === 'granted') {
-      const notification = new Notification('Bem vindo de volta!', {
-        body: 'Que bom que voltou!',
-      });
-    } else {
-      console.log('Permission denied');
-    }
-  });
-};
-
-// Envia notificação personalizada
-btnCustom.addEventListener('click', event => {
-  event.preventDefault();
-  Notification.requestPermission().then(perm => {
-    if (perm === 'granted') {
-      const notification = new Notification(title.value, {
-        body: message.value,
-        icon: icon.value,
-        image: image.value,
-      });
-    } else {
-      console.log('Permission denied');
-    }
-  });
-});
+// // Envia notificação personalizada
+// btnCustom.addEventListener('click', event => {
+//   event.preventDefault();
+//   Notification.requestPermission().then(perm => {
+//     if (perm === 'granted') {
+//       const notification = new Notification(title.value, {
+//         body: message.value,
+//         icon: icon.value,
+//         image: image.value,
+//       });
+//     } else {
+//       console.log('Permission denied');
+//     }
+//   });
+// });
